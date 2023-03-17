06f8a8b3c3d559584fc14a1023931e96e0d7bf93
Szymon Szczurowski <szczurowski.szymon@wp.pl>
2023-03-17 16:10:33 +0100

    adding api and enter function call

--- projects/Magic8ball/main.js
+++ projects/Magic8ball/main.js
@@ -3,57 +3,80 @@ const ballImg = document.querySelector('.img')
 const answer = document.querySelector('.answer')
 const error = document.querySelector('.error')
 
-const answersArr = ['Tak!', 'Nie.', 'Może.', 'Ciężko powiedzieć...', 'Nie chcesz znać odpowiedzi na to pytanie... :/']
-
-// W prszysłości pobrać z API jak będzie mnie stać
+// const answersArr = ['Tak!', 'Nie.', 'Może.', 'Ciężko powiedzieć...', 'Nie chcesz znać odpowiedzi na to pytanie... :/']
+// DODAĆ API!
 
 const shakeBall = () => {
 	ballImg.classList.add('shake-animation')
-    setTimeout(checkInput, 1000)
+	setTimeout(checkInput, 1000)
 }
 
 const checkInput = () => {
 	const text = input.value
 
 	if (text === '') {
-		error.textContent = 'Zadaj pytanie!'
+		error.textContent = 'You have to ask the question!'
 		answer.textContent = ''
 	} else if (text.length < 10) {
-		error.textContent = 'Twoje zdanie powinno mieć min 10 znaków'
+		error.textContent = 'Your question should have a minimum of 10 characters'
 		answer.textContent = ''
-		// else if (text.slice(-1) !== '?)
 	} else if (!text.endsWith('?')) {
-		error.textContent = 'Pytanie musi być zakończone znakiem "?"'
+		// else if (text.slice(-1) !== '?)
+		error.textContent = 'The question must end with a "?"'
 		answer.textContent = ''
 	} else {
 		error.textContent = ''
-		generateAnswer()
+		// generateAnswer()
+		generateApiAnswer(text)
 	}
-    ballImg.classList.remove('shake-animation')
-
+	ballImg.classList.remove('shake-animation')
 }
 
-const generateAnswer = () => {
-	const radnomNumber = Math.floor(Math.random() * answersArr.length)
+const generateApiAnswer = input => {
 
-	// answer.textContent = answersArr[radnomNumber]
-	answer.innerHTML = `<span>Odpowiedź:</span> ${answersArr[radnomNumber]}`
+	const options = {
+	  method: 'GET',
+	  url: 'https://magic-8-ball1.p.rapidapi.com/my_answer/',
+	  params: {question: 'input'},
+	  headers: {
+	    'X-RapidAPI-Key': '138f7b131emshae28ee3e133cdc2p178a3djsn25b317e33ff6',
+	    'X-RapidAPI-Host': 'magic-8-ball1.p.rapidapi.com'
+	  }
+	};
+	axios.request(options).then(function (response) {
+		answer.innerHTML = `<span>Answer:</span> ${response.data.answer}`
+	}).catch(function (error) {
+		console.error(error);
+	});
 }
 
+const enterKeyCheck = (e) => {
+	if(e.key === 'Enter'){
+		shakeBall()
+	}
+}
+input.addEventListener('keyup', enterKeyCheck)
 ballImg.addEventListener('click', shakeBall)
+
+
+// const generateAnswer = () => {
+// 	const radnomNumber = Math.floor(Math.random() * answersArr.length)
+// 	// answer.textContent = answersArr[radnomNumber]
+// 	answer.innerHTML = `<span>Odpowiedź:</span> ${answersArr[radnomNumber]}`
+// }
 
 
 // const checkInput = () => {
 //     if (input.value !== '' && input.value.slice(-1) === '?') {
 //         generateAnswer();
-//         error.textContent = '';       
+//         error.textContent = '';
 //     } else if (input.value !== '' && input.value.slice(-1) !== '?') {
 //         error.textContent = 'Pytanie musi być zakończone znakiem "?".';
-//         answer.textContent = ''        
+//         answer.textContent = ''
 //     } else {
 //         error.textContent = 'Musisz zadać jakieś pytanie!';
-//         answer.textContent = ''  
+//         answer.textContent = ''
 //     }
-    
-//     ball.classList.remove('shake-animaton');      
+
+//     ball.classList.remove('shake-animaton');
 // }
