0c79a75d3f403bdfeb98ff069c468c6981b3ba5b
Szymon Szczurowski <szczurowski.szymon@wp.pl>
2023-03-28 14:32:08 +0200

    Exchange rate app 0.1

---
+++ projects/ExchangeRateApp/main.js
@@ -0,0 +1,79 @@
+const swapBtn = document.querySelector('.swap')
+let currencyOne = document.querySelector('#currency-one')
+let currencyTwo = document.querySelector('#currency-two')
+const amountOne = document.querySelector('.amount-one')
+const amountTwo = document.querySelector('.amount-two')
+const rateInfo = document.querySelector('.rate-info')
+
+const API_KEY = 'JksyVbhQ9rNEKORlxwDfuR750vzk3RFH'
+
+// const calcualte = () => {
+// 	const currencyTo = currencyTwo.value
+// 	const currencyFrom = currencyOne.value
+// 	const amount = amountOne.value
+
+// 	let myHeaders = new Headers();
+// 	myHeaders.append("apikey", API_KEY);
+
+// 	let requestOptions = {
+// 	  method: 'GET',
+// 	  redirect: 'follow',
+// 	  headers: myHeaders
+// 	};
+
+// 	fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${currencyTo}&from=${currencyFrom}&amount=${amount}`, requestOptions)
+// 	  .then(response => response.json())
+// 	  .then(data =>  {
+// 	   rateInfo.textContent  = `${amountOne.value} ${currencyFrom} = ${data.result.toFixed(4)} ${currencyTo}`
+// 	   amountTwo.value = data.result.toFixed(2)
+
+// 	})
+// 	  .catch(error => console.log('error', error));
+// }
+
+
+// const synchronizeCurrencies = () => {
+//     const temp = currencyOne.value;
+//     currencyOne.value = currencyTwo.value;
+//     currencyTwo.value = temp;
+// }
+
+// calcualte()
+// currencyOne.addEventListener('change', calcualte)
+// currencyTwo.addEventListener('change', calcualte)
+// amountOne.addEventListener('input', calcualte)
+// swapBtn.addEventListener('click', () => {
+//     synchronizeCurrencies()
+//     calcualte()
+// })
+
+
+
+// WERSJA 2
+
+
+
+// const calcualte2 = () => {
+// 	let myHeaders = new Headers();
+// 	myHeaders.append("apikey", API_KEY);
+
+// 	let requestOptions = {
+// 	  method: 'GET',
+// 	  redirect: 'follow',
+// 	  headers: myHeaders
+// 	};
+
+// 	fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${currencyTwo.value}&from=${currencyOne.value}&amount=${amountOne.value}`, requestOptions)
+
+// 	  .then(response => response.json())
+// 	  .then(data =>  {
+//         console.log(data);
+// 	//    rateInfo.textContent  = `${amountOne.value} ${currencyFrom} = ${data.result.toFixed(4)} ${currencyTo}`
+// 	//    amountTwo.value = data.result.toFixed(2)
+    
+
+// 	})
+// 	  .catch(error => console.log('error', error));
+// }
+
+// calcualte2()
